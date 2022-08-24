2d9e7edabfddffce8706170e3796d56799cf359a
sweetie <num1carbon@gmail.com>
2022-08-24 13:26:23 +0300

    added controls to popups

--- components/CartPopup.vue
+++ components/CartPopup.vue
@@ -2,10 +2,35 @@
   <div class="wrapper">
     <transition-group name="list" mode="out-in">
       <div class="popup" v-for="item in queue" :key="item.name">
-        <div class="popup__title">Товар добавлен в корзину</div>
         <img :src="item.imageUrl" class="popup__image" />
-        <div class="popup__name">{{ item.name }}</div>
-        <!-- TODO: кнопка закрыть и перейти в корзину -->
+        <div class="popup__desc">
+          <div class="popup__title">Товар добавлен в корзину</div>
+          <div class="popup__name">{{ item.name }}</div>
+        </div>
+        <div class="popup__controls">
+          <button class="popup__close" @click="close">
+            <svg
+              width="24px"
+              height="24px"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g data-name="Layer 2">
+                <g data-name="close">
+                  <rect
+                    width="24"
+                    height="24"
+                    transform="rotate(180 12 12)"
+                    opacity="0"
+                  />
+                  <path
+                    d="M13.41 12l4.3-4.29a1 1 0 1 0-1.42-1.42L12 10.59l-4.29-4.3a1 1 0 0 0-1.42 1.42l4.3 4.29-4.3 4.29a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0l4.29-4.3 4.29 4.3a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42z"
+                  />
+                </g>
+              </g>
+            </svg></button
+          ><nuxt-link to="/cart" class="popup__go-to-cart">К корзине</nuxt-link>
+        </div>
       </div></transition-group
     >
   </div>
@@ -16,7 +41,8 @@ export default {
   // TODO: показывать если товар уже в корзине
   data: () => ({
     queue: [],
-    showTime: 2000,
+    showTime: 200000,
+    timer: null,
   }),
 
   props: {
@@ -26,7 +52,11 @@ export default {
   methods: {
     show(name, url) {
       this.queue.push({ name, imageUrl: url });
-      setTimeout(() => this.queue.shift(), this.showTime);
+      this.timer = setTimeout(() => this.queue.shift(), this.showTime);
+    },
+    close() {
+      this.queue.shift();
+      clearTimeout(this.timer);
     },
   },
 };
@@ -49,7 +79,7 @@ export default {
 }
 
 .wrapper {
-  width: 600px;
+  width: 500px;
 
   display: flex;
   flex-direction: column;
@@ -64,7 +94,14 @@ export default {
 .popup {
   width: 100%;
   height: 100px;
+  padding: 10px;
+  margin: 0 0 15px 0;
+
   background: $light;
+  box-shadow: 0 6px 6px rgba(black, 0.2);
+
+  display: flex;
+  justify-content: space-between;
 
   &__title {
     font-size: 1.2em;
@@ -74,7 +111,36 @@ export default {
   }
 
   &__image {
-    width: 50px;
+    display: block;
+    width: 60px;
+    height: 100%;
+  }
+
+  &__desc {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+  }
+
+  &__controls {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    justify-content: space-around;
+  }
+
+  &__close,
+  &__go-to-cart {
+    border: none;
+    cursor: pointer;
+  }
+
+  &__go-to-cart {
+    font-weight: 600;
+    color: white;
+    background-color: $blue;
+    padding: 5px 8px;
+    text-decoration: none;
   }
 }
 </style>
