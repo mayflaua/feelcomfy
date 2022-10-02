d2bef072673214dab35c486578418873e0a71a43
Danil <num1carbon@gmail.com>
2022-10-03 00:16:43 +0300

    titled wrapper component

---
+++ components/UI/TitledWrapper.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="wrapper">
+    <nuxt-link :to="props.path" class="wrapper__title">
+      {{ title }}
+      <svg
+        class="arrow"
+        data-v-ca056c68=""
+        fill="none"
+        height="36"
+        viewBox="0 0 24 24"
+        width="36"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          clip-rule="evenodd"
+          d="M9.18945 16.4983C8.91426 16.1887 8.94215 15.7146 9.25174 15.4394L13.1211 12L9.25173 8.56055C8.94215 8.28536 8.91426 7.81131 9.18945 7.50172C9.46464 7.19213 9.93869 7.16425 10.2483 7.43943L14.7483 11.4394C14.9084 11.5818 15 11.7858 15 12C15 12.2142 14.9084 12.4182 14.7483 12.5605L10.2483 16.5605C9.93869 16.8357 9.46464 16.8079 9.18945 16.4983Z"
+          fill="black"
+          fill-rule="evenodd"
+        />
+      </svg>
+    </nuxt-link>
+    <slot />
+  </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+  path: {
+    type: String,
+    default: ''
+  },
+  title: {
+    type: String,
+    default: 'Заголовок'
+  }
+})
+
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  width: 100%;
+  height: fit-content;
+  margin: 30px 0;
+
+  &__title {
+    width: fit-content;
+    margin: 0 0 30px 0;
+
+    font-size: 1.6rem;
+    font-weight: 500;
+    color: $font;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+    gap: 5px;
+
+    cursor: pointer;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+}
+</style>
