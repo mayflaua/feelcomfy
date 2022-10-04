0ced254e2863c49a6dba305ea357b27be455dbfe
Danil <num1carbon@gmail.com>
2022-10-04 21:06:38 +0300

    add fetchSuggestion to useSearch composable

--- composables/useSearch.ts
+++ composables/useSearch.ts
@@ -26,8 +26,32 @@ const useSearch = () => {
 
     return limit ? res.slice(limit) : res
   }
+
+  const fetchSuggestions = async (query: string): Promise<string[]> => {
+    // find products matches based on title (title contains query)
+    const { data: matchedByTitle } = await supabase
+      .from('goods')
+      .select('title')
+      .ilike('title', `%${query}%`)
+
+    // find products matches based on fts vector
+    const { data: matchedByFts } = await supabase
+      .from('goods')
+      .select('pk_id, title, old_price, final_price, netlify_name, model, color,' +
+        ' orders')
+      .textSearch('title', query, { config: 'russian', type: 'websearch' })
+
+    // concat results and remove duplicates
+    const res = matchedByFts.concat(matchedByTitle).filter((value, index, arr) =>
+      index === arr.findIndex(temp =>
+        temp.title === value.title
+      )
+    ).map(obj => obj.title)
+    return res
+  }
   return {
-    findByQuery
+    findByQuery,
+    fetchSuggestions
   }
 }
 
