c7477486374973549186486c361fd62f2d90d394
Danil <num1carbon@gmail.com>
2022-10-24 00:59:41 +0300

    fix to prevent layout shift in banner carousel

---
+++ components/Admin/DataFields.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="data-field">
+    <span class="data-field__label">Поле:</span>
+    <input class="data-field__label-input" type="text" ui-input>
+    <span class="data-field__value">Значение: </span>
+    <input class="data-field__value-input" type="text" ui-input>
+  </div>
+</template>
+
+<script setup>
+const fields = reactive({})
+</script>
+
+<style lang="scss" scoped>
+.data-field {
+  display: grid;
+  grid-template-columns: 100px 1fr;
+  grid-template-rows: 1fr 1fr;
+}
+</style>
--- components/Index/BannerCarousel.vue
+++ components/Index/BannerCarousel.vue
@@ -1,32 +1,47 @@
 <template>
-  <Swiper
-    :autoplay="{
-      delay: 3500,
-      pauseOnMouseEnter: true,
-    }"
-    :initial-slide="2"
-    :loop="true"
-    :modules="modules"
-    :navigation="true"
-    :slides-per-view="1"
-    class="banner-carousel"
-  >
-    <SwiperSlide v-for="(banner, i) in banners" :key="banner.image">
-      <nuxt-link :to="banner.path" no-prefetch>
-        <nuxt-img
-          :alt="banner.alt"
-          :loading="i !== 2 ? 'lazy' : 'eager'"
-          :preload="i === 2"
-          :src="banner.image"
-          format="webp"
-          height="400"
-          quality="90"
-          sizes="xxl:1400px xl:1100px lg:900px md:650px sm:500px xs:200px"
-          width="990"
-        />
-      </nuxt-link>
-    </SwiperSlide>
-  </Swiper>
+  <div class="wrapper">
+    <nuxt-img
+      v-if="!_swiperInitialized"
+      :src="banners[2].image"
+      class="image--fallback"
+      format="webp"
+      height="400"
+      preload
+      quality="40"
+      sizes="xxl:1400px xl:1100px lg:900px md:650px sm:500px xs:200px"
+      width="990"
+    />
+    <Swiper
+      v-show="_swiperInitialized"
+      :autoplay="{
+        delay: 3500,
+        pauseOnMouseEnter: true,
+      }"
+      :initial-slide="2"
+      :loop="true"
+      :modules="modules"
+      :navigation="true"
+      :slides-per-view="1"
+      class="banner-carousel"
+      @init="handleSwiperInit"
+    >
+      <SwiperSlide v-for="(banner, i) in banners" :key="banner.image">
+        <nuxt-link :to="banner.path" no-prefetch>
+          <nuxt-img
+            :alt="banner.alt"
+            :loading="i !== 2 ? 'lazy' : 'eager'"
+            :preload="i === 2"
+            :src="banner.image"
+            format="webp"
+            height="400"
+            quality="90"
+            sizes="xxl:1400px xl:1100px lg:900px md:650px sm:500px xs:200px"
+            width="990"
+          />
+        </nuxt-link>
+      </SwiperSlide>
+    </Swiper>
+  </div>
 </template>
 
 <script setup>
@@ -38,11 +53,21 @@ import 'swiper/css/navigation'
 import 'swiper/css/autoplay'
 
 const modules = [Navigation, Autoplay]
-
+const _swiperInitialized = ref(false)
+const handleSwiperInit = () => {
+  _swiperInitialized.value = true
+}
 const { data: banners } = await useFetch('/api/banners')
 </script>
 
 <style lang="scss" scoped>
+
+.image--fallback {
+  width: 100%;
+  height: 100%;
+  border-radius: 20px;
+}
+
 .banner-carousel {
   width: 100%;
   margin: 0 auto;
